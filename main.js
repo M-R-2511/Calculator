8902cfa2686698108a3c135b40989757c9984fea
mohamed1973465 <mr2837465@gmail.com>
2022-02-14 12:58:14 +0200

    function result(),style-table

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-let input = document.querySelector("input");
-
+let input = document.querySelector(".input");
+let result = document.getElementById("result");
 function element(e) {
   input.value += e.innerHTML;
 }
 
 function solve() {
-  try {
-    input.value = eval(input.value);
-  } catch {
-    input.value = "Error!";
+  if (input.value.trim() != "") {
+    try {
+      result.value = eval(input.value);
+    } catch {
+      result.value = "Error!";
+    }
   }
+  input.value = "";
 }
 
 function clr() {
   input.value = "";
+  result.value = "";
 }
 
 function del() {
   let x = input.value[length - 1];
   if (x === " ") {
     input.value = input.value.slice(0, -2);
+    result.value = result.value.slice(0, -2);
   } else {
     input.value = input.value.slice(0, -1);
+    result.value = result.value.slice(0, -1);
   }
+}
+
+function backResult() {
+  input.value = result.value;
 }
